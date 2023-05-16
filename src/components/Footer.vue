7562a0ec7a3daf64606f48013807733df33a01de
Kroissan <nathankr@free.fr>
2023-05-16 16:31:06 +0200

    removed dark mode

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { useRouter } from 'vue-router'
 import { useAuthenticated, useSignOut } from '@nhost/vue'
-import { isDark, toggleDark } from '~/composables'
 
 const isAuthenticated = useAuthenticated()
 const { signOut } = useSignOut()
@@ -14,11 +13,6 @@ const handleSignOut = () => {
 
 <template>
   <nav text-xl mt-6 inline-flex gap-2>
-    <button class="icon-btn !outline-none" @click="toggleDark()">
-      <div v-if="isDark" i-carbon-moon />
-      <div v-else i-carbon-sun />
-    </button>
-
     <button v-if="isAuthenticated" class="icon-btn !outline-none" @click="handleSignOut">
       <div i-carbon-logout />
     </button>
