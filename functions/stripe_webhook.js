de31b26f96b0d07e5f217bbf70d6470223529655
Kroissan <nathankr@free.fr>
2023-06-02 09:59:50 +0200

    notification on trip booked

--- functions/stripe_webhook.js
+++ functions/stripe_webhook.js
@@ -12,16 +12,18 @@ const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET;
 const SET_PAYMENT = `mutation setPayment($id: uuid!) {
   update_bookings_by_pk(pk_columns: {id: $id}, _set: {payment_status: "success"}) {
     trip {
-      driver_id
+      id
+      user {
+        balance {
+          id
+          balance
+        }
+      }
     }
-  }
-}
-`;
-
-const GET_BALANCE = `query getBalance($id: uuid!) {
-  balances(where: {user_id: {_eq: $id}}) {
-    id
-    balance
+    user {
+      displayName
+    }
+    seats_booked
   }
 }
 `;
@@ -33,9 +35,18 @@ const INSERT_BALANCE = `mutation insertBalance($user_id: uuid!, $balance: Int!) 
 }
 `;
 
-const UPDATE_BALANCE = `mutation updateBalance($user_id: uuid!, $balance: Int!) {
+const UPDATE_BALANCE = `mutation updateBalance($id: uuid!, $balance: Int!) {
   update_balances_by_pk(pk_columns: {id: $id}, _set: {balance: $balance}){
     id
+  }
+}
+`;
+
+const INSERT_NOTIFICATION = `mutation InsertNotifications($message: String, $trip_id: String, $user_id: String) {
+  insert_notifications(objects: {message: $message, trip_id: $trip_id, user_id: $user_id}) {
+    returning {
+      id
+    }
   }
 }
 `;
@@ -56,31 +67,30 @@ export default async (req, res) => {
   // Handle the event
   if(event.type === 'checkout.session.completed') {
       const booking = event.data.object.client_reference_id;
-      const {data: mutation, error: mutationError} = await nhost.graphql.request(SET_PAYMENT, {
+      const {data, error} = await nhost.graphql.request(SET_PAYMENT, {
         id: booking,
       });
-      if (mutationError) {
-        console.log(mutationError)
+      if (error) {
+        console.log(error)
         return res.status(500).json({ error: "Error updating booking" });
       }
-      const {data: balance, error: balanceErrror} = await nhost.graphql.request(GET_BALANCE, {
-        id: mutation.update_bookings_by_pk.trip.driver_id,
-      });
-      if (balanceErrror) {
-        console.log(balanceErrror)
-        return res.status(500).json({ error: "Error getting driver balance" });
-      }
-      if (balance.balances.length === 0){
+      
+      if (data.update_bookings_by_pk.trip.user.balance.length === 0){
         const {data: insert, error: insertError} = await nhost.graphql.request(INSERT_BALANCE, {
           user_id: mutation.update_bookings_by_pk.trip.driver_id,
           balance: event.data.object.amount_total / 100,
         });
       }else{
         const {data: update, error: updateError} = await nhost.graphql.request(UPDATE_BALANCE, {
-          id: balance.balances[0].id,
-          balance: balance.balances[0].balance + event.data.object.amount_total / 100,
+          id: data.update_bookings_by_pk.trip.user.balance.id,
+          balance: data.update_bookings_by_pk.trip.user.balance.balance + event.data.object.amount_total / 100,
         });
       }
+      const {data: notification, error: notificationError} = await nhost.graphql.request(INSERT_NOTIFICATION, {
+        message: `${data.update_bookings_by_pk.user.displayName} souhaite réserver ${data.update_bookings_by_pk.seats_booked} places pour votre trajet`,
+        trip_id: data.update_bookings_by_pk.trip.id,
+        user_id: data.update_bookings_by_pk.trip.driver_id,
+      });
   }
   else {
       console.log(`Unhandled event type ${event.type}`);
